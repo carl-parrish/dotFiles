97cceacc59c606c0405976e37312d3b568e4a8be
Carl Parrish <carl.parrish@gmail.com>
2025-11-19 14:02:00 -0700

    chore: make fnm and rustup conf.d scripts portable

--- fish/.config/fish/conf.d/fnm.fish
+++ fish/.config/fish/conf.d/fnm.fish
@@ -1,1 +1,5 @@
-fnm env --use-on-cd --shell fish | source
+# fnm.fish - auto-enable fnm if installed
+
+if type -q fnm
+    fnm env --use-on-cd --shell fish | source
+end
--- fish/.config/fish/conf.d/rustup.fish
+++ fish/.config/fish/conf.d/rustup.fish
@@ -1,1 +1,6 @@
-source "$HOME/.cargo/env.fish"
+# rustup.fish - source cargo env if present
+
+set cargo_env "$HOME/.cargo/env.fish"
+if test -f $cargo_env
+    source $cargo_env
+end
