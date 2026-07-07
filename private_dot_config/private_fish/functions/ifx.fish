8132f0b94b3333525495acbd4ef72abe79789bc9
Carl Parrish <carl.parrish@gmail.com>
2026-07-07 14:29:08 -0700

    feat(abbr): add ifx function with project id built in

---
+++ private_dot_config/private_fish/functions/ifx.fish
@@ -0,0 +1,14 @@
+function ifx --description "Run a command with Infisical secrets"
+    set -l env_name $argv[1]
+    set -l cmd $argv[2..-1]
+
+    if test -z "$env_name"
+        echo "Usage: ifx <env> <command...>"
+        return 1
+    end
+
+    infisical run \
+        --env "$env_name" \
+        --projectId "$INFISICAL_PROJECT_ID" \
+        -- $cmd
+end
