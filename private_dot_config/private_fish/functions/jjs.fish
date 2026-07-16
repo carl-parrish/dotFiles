36752c972e6cdfbc0853c6e3e53f10a756e52ebf
Carl Parrish <carl.parrish@gmail.com>
2026-07-15 19:00:52 -0700

    refactor(jjs): updates to make jj workspaces smoother

--- private_dot_config/private_fish/functions/jjs.fish
+++ private_dot_config/private_fish/functions/jjs.fish
@@ -1,23 +1,19 @@
 function jjs
-    # 1. Use native jj list + fzf to select an existing workspace path dynamically
-    # We grab the path (column 2) from the workspace picked in fzf
-    set -l target_dir (jj workspace list | fzf --height 40% --reverse --prompt="Switch Workspace > " | awk '{print $2}')
-    
-    # Exit gracefully if fzf is escaped without picking anything
-    if test -z "$target_dir"
+    set -l selected (jj workspace list | fzf --height 40% --reverse --prompt="Switch Workspace > ")
+
+    if test -z "$selected"
         return
     end
 
-    # 2. Extract the directory name to use as a clean session name
-    set -l session_name (basename $target_dir)
+    # Preserve paths containing spaces: remove the `workspace-name:` prefix.
+    set -l target_dir (string replace -r '^[^:]+:\s*' '' -- "$selected")
+    set -l session_name (basename "$target_dir")
 
-    # 3. Change directory to the target workspace
-    cd $target_dir
+    cd "$target_dir"; or return
 
-    # 4. Attach to the Zellij session if it exists, or spawn a new one in that directory
     if zellij list-sessions | string match -r "^$session_name\s" > /dev/null
-        zellij attach $session_name
+        zellij attach "$session_name"
     else
-        # Starts Zellij mapped directly to the workspace dir
-        zellij --session $session_name options --default-cwd $target_dir
+        zellij --session "$session_name" options --default-cwd "$target_dir"
     end
+end
